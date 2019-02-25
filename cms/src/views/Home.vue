bc82c142484114cc80b03741b17ae2c37f9b2922
Nathan <497948679@qq.com>
2019-02-25 17:19:25 +0800

    创建Vue项目

---
+++ cms/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <p>
+      123
+      {{abc}}
+    </p>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  data: ()=>{
+    return {
+      abc: 321
+    }
+  },
+}
+</script>
